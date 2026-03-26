362d102bba21a489b25c81d3c717173e48bb6b5a
Pavan Raghuwanshi <pavanraghuwanshi77@gmail.com>
2026-03-26 10:11:54 +0530

    fix cors error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import { Hono } from "hono";
 import userRoutes from "../src/modules/user/user.route";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+// ✅ Allow ALL CORS
+app.use("*", cors({
+  origin: "*",              // sab allow
+  allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowHeaders: ["*"],
+}));
 
 app.get("/", (c) => {
   return c.json({ message: "CRM API running 🚀" });
