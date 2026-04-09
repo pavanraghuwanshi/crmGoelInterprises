830ae9582cd91ae806320c924cf9bed36790fb2d
Pavan Raghuwanshi <pavanraghuwanshi77@gmail.com>
2026-04-10 00:28:15 +0530

    issue fix

--- src/modules/Roaster/roster.route.ts
+++ src/modules/Roaster/roster.route.ts
@@ -1,7 +1,11 @@
 import { Hono } from "hono";
 import { assignAttendancePolicyBulk } from "./roster.controller";
+import { verifyToken } from "../../middleware/auth.middleware";
 
 const rosterRoute = new Hono();
+
+rosterRoute.use("*", verifyToken);
+
 
 rosterRoute.post("/assign-attendance-policy", assignAttendancePolicyBulk);
 
