a6e44930dd62bba154e3ca4d7bfada3f41dcca49
Gagan Raghuwanshi <gaganraghuwanshi77@gmail.com>
2026-04-19 15:48:52 +0530

    leave auth fix

--- src/modules/leaveManagement/leave.route.ts
+++ src/modules/leaveManagement/leave.route.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
 import { applyLeave, getLeaves, getLeavesByUserId, updateLeaveStatus } from "./leave.controller";
+import { verifyToken } from "../../middleware/auth.middleware";
 
 const leaveRoutes = new Hono();
 
 leaveRoutes.post("/",applyLeave );
+leaveRoutes.use("*", verifyToken);
+
 leaveRoutes.get("/",getLeaves );
 leaveRoutes.get("/by-user",getLeavesByUserId );
 leaveRoutes.put("/:id",updateLeaveStatus );
